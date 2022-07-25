54a47722ef7c035d3e6db019de0dd93f99cc7a85
Fezz <43589668+FBFezz@users.noreply.github.com>
2022-07-25 02:59:56 +0100

    Optimisations

--- esx_advanced_duty/server/main.lua
+++ esx_advanced_duty/server/main.lua
@@ -1,26 +1,20 @@
-ESX = nil
-
-TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
-
 RegisterServerEvent('esx_advanced_duty:changeDutyStatus') 
 AddEventHandler('esx_advanced_duty:changeDutyStatus', function()
-
-    local _source = source
-    local xPlayer = ESX.GetPlayerFromId(_source)
+    local source = source
+    local xPlayer = ESX.GetPlayerFromId(source)
 
     if xPlayer.job.name then
-
         local jobName = xPlayer.job.name 
 
         if string.match(jobName, "off") then 
             jobName = jobName:gsub("%off", "") xPlayer.setJob(jobName, xPlayer.job.grade)
 
-            TriggerClientEvent('esx:showNotification', _source, _U('onduty'))
+            TriggerClientEvent('esx:showNotification', source, _U('onduty'))
             return
         else
             xPlayer.setJob('off' .. jobName, xPlayer.job.grade)
 
-            TriggerClientEvent('esx:showNotification', _source, _U('offduty'))
+            TriggerClientEvent('esx:showNotification', source, _U('offduty'))
         end
     else
         print("[Error] Player has no job to perform this action.")
